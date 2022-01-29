f35855598f148f145f38fc3ba68a91329ecda072
Lucas Alves <lucasalvesf78@gmail.com>
2022-01-28 22:54:19 -0300

    Testando replace

--- teste.py
+++ teste.py
@@ -1,4 +1,8 @@
-# lista = ['a','b','c','a']
-lista = ['a','a','a','a','a']
-print(lista.count('a'))
-print(len(set(lista)))
+# # lista = ['a','b','c','a']
+# lista = ['a','a','a','a','a']
+# print(lista.count('a'))
+# print(len(set(lista)))
+
+my_string = r'\\ServerA\DriveB\5.FolderC\A.TXT'
+my_string = my_string.replace('\\', '/')
+print(my_string)
